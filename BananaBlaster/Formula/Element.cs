d39c054988a33c7931033849f8606b1f79277c84
Uhkko <113055120+Uhkko@users.noreply.github.com>
2026-03-14 15:30:39 +0100

    Generalize element hash codes

--- BananaBlaster/Formula/Element.cs
+++ BananaBlaster/Formula/Element.cs
@@ -5,6 +5,16 @@ namespace BananaBlaster.Formula;
 public abstract class Element
 {
     public abstract Element[] Children { get; }
+    protected abstract object[] Identifiers { get; }
     
     public abstract BoolExpr CreateConstraint(BBContext context);
+
+    public override int GetHashCode()
+    {
+        var hash = new HashCode();
+        foreach (var identifier in Identifiers)
+            hash.Add(identifier);
+        hash.Add(GetType());
+        return Math.Abs(hash.ToHashCode());
+    }
 }
